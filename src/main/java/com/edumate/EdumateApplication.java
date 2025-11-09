f74bbe6877c4a82d238a48512b331228b1ab98e9
jun <jwjjwj1960@naver.com>
2025-11-09 22:49:04 +0900

    set-up

---
+++ src/main/java/com/edumate/EdumateApplication.java
@@ -0,0 +1,13 @@
+package com.edumate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdumateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdumateApplication.class, args);
+	}
+
+}
