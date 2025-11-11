46eea72c61d85169d096a5195a8465bccf92c9b5
jun <jwjjwj1960@naver.com>
2025-11-11 23:22:53 +0900

    style: geminiClient 변경으로 코드 변경 후 구동테스트

--- src/main/java/com/edumate/Main.java
+++ src/main/java/com/edumate/Main.java
@@ -1,32 +1,19 @@
 package com.edumate;
 
 
-import com.edumate.domain.chat.ChatResponse;
 import com.edumate.io.GeminiClient;
 import com.edumate.io.InputHandler;
 import java.io.IOException;
-import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
+import java.util.Optional;
 
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
-        HttpClient client = HttpClient.newHttpClient();
-
+        GeminiClient geminiClient = new GeminiClient();
         //String prompt = "자바의 StringBuilder에 대한 간단한 퀴즈를 내줘.";
         String prompt = InputHandler.readInput();
-
-        String requestBody = String.format("""
-                {
-                    "contents": [{
-                        "parts": [{"text": "%s"}]
-                    }]
-                }
-                """, prompt);
-        HttpRequest request = GeminiClient.contact(prompt,requestBody);
-
-        String response = ChatResponse.responseChat(client, request);
+        Optional<String> response = geminiClient.getResponse(prompt);
         System.out.println(response);
     }
 }
