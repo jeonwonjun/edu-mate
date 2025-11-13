e62e6f5ec027d6ac4f2b144d6a51c7f8e28df16b
jun <jwjjwj1960@naver.com>
2025-11-13 19:34:42 +0900

    feat: buildRequestBody 변경
    기존 Json을 통째로 받는 구조에서 Gson으로 구분해서 받는 구조로 변경

--- src/main/java/com/edumate/io/GeminiClient.java
+++ src/main/java/com/edumate/io/GeminiClient.java
@@ -37,13 +37,22 @@ public class GeminiClient {
     }
 
     private String buildRequestBody(String prompt) {
-        return String.format("""
-                {
-                    "contents": [{
-                        "parts": [{"text": "%s"}]
-                    }]
-                }
-                """, prompt);
+        JsonObject textPart = new JsonObject();
+        textPart.addProperty("text", prompt);
+
+        JsonArray partsArray = new JsonArray();
+        partsArray.add(textPart);
+
+        JsonObject contentObj = new JsonObject();
+        contentObj.add("parts", partsArray);
+
+        JsonArray contentsArray = new JsonArray();
+        contentsArray.add(contentObj);
+
+        JsonObject root = new JsonObject();
+        root.add("contents", contentsArray);
+
+        return root.toString();
     }
 
     private HttpRequest buildRequest(String requestBody){
