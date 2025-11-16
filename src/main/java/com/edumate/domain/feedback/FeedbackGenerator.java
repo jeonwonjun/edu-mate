522f8fb3ab706feebb3e57da915e70ffb7a1c720
jun <jwjjwj1960@naver.com>
2025-11-16 14:58:47 +0900

    style: prompt 수정
    prompt engineering을 통해 사용자 피드백 퀄리티 향상
    gemini에게 역할을 더 확실히 부여하여 어색했던 피드백을 조절

--- src/main/java/com/edumate/domain/feedback/FeedbackGenerator.java
+++ src/main/java/com/edumate/domain/feedback/FeedbackGenerator.java
@@ -1,5 +1,6 @@
 package com.edumate.domain.feedback;
 
+import com.edumate.domain.quiz.AnswerChecker;
 import com.edumate.domain.quiz.Quiz;
 import com.edumate.io.GeminiClient;
 import java.net.http.HttpRequest;
@@ -13,7 +14,7 @@ public class FeedbackGenerator {
     }
 
     public Feedback generate(Quiz quiz, String userAnswer) {
-        boolean isCorrect = quiz.checkAnswer(userAnswer);
+        boolean isCorrect = quiz.isCorrect(userAnswer);
 
         if (isCorrect) {
             return new Feedback(true, "정답입니다! 잘 이해하고 있어요");
@@ -27,7 +28,10 @@ public class FeedbackGenerator {
 
     private String buildFeedbackPrompt(Quiz quiz, String userAnswer) {
         return """
-                당신은 스탠포드 CS 교수입니다.
+                당신은 객관식 퀴즈에 대해 해설을 작성하는 교사 역할의 AI입니다.
+                사용자의 목적은 학생에게 '왜 정답이 이것인지'와 '왜 학생의 선택이 틀렸는지'를
+                명확하게 이해시키는 것입니다.
+                
                 아래는 학생의 퀴즈 답변에 정보입니다.
                 학생이 오답을 제출했다면, 이유를 간단히 설명하고 올바른 개념을 정리해 주세요.
                 너무 장황하지 않게, 핵심만 간결히 설명해 주세요.
@@ -37,8 +41,6 @@ public class FeedbackGenerator {
                 학생의 답변: %s
                 정답: %s
                 
-                출력 형식:
-                - 한 문장으로 핵심 피드백
                 """.formatted(quiz.getOptions(), quiz.getOptions(), userAnswer, quiz.getAnswer());
     }
 }
