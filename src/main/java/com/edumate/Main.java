99217da9d3df146496da4470bead5935cc433c9d
전원준 <jwjjwj1960@naver.com>
2025-11-23 19:43:11 +0900

    feat: AppController 동작
    Main에서 AppController 호출 해서 run

--- src/main/java/com/edumate/Main.java
+++ src/main/java/com/edumate/Main.java
@@ -1,36 +1,12 @@
 package com.edumate;
 
 
-import com.edumate.domain.feedback.Feedback;
-import com.edumate.domain.feedback.FeedbackGenerator;
-import com.edumate.domain.quiz.Quiz;
-import com.edumate.domain.quiz.QuizGenerator;
-import com.edumate.io.GeminiClient;
-import com.edumate.io.InputHandler;
-import com.edumate.io.OutputHandler;
-import com.edumate.util.Information;
+import com.edumate.controller.AppController;
 import java.io.IOException;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
-
-        GeminiClient client = new GeminiClient();
-        QuizGenerator quizGenerator = new QuizGenerator(client);
-        FeedbackGenerator feedbackGenerator = new FeedbackGenerator(client);
-        System.out.println(Information.INPUT_QUESTION.getMessage());
-        String question = InputHandler.read();
-
-        List<Quiz> quizzes = quizGenerator.generate(question, 3);
-
-        for (Quiz quiz : quizzes) {
-            OutputHandler.printQuiz(quiz);
-
-            System.out.print("답변: ");
-            String userAnswer = InputHandler.read();
-            Feedback feedback = feedbackGenerator.generate(quiz, userAnswer);
-
-            OutputHandler.printFeedback(feedback);
-        }
+        AppController appController = new AppController();
+        appController.run();
     }
 }
